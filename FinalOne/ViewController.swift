f79c5f744f1540069d4bb52895c670ed2a7636af
Jimmy Liddle <jameslid12@msn.com>
2018-09-22 23:36:39 +0100

    Initial Commit

---
+++ FinalOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalOne
+//
+//  Created by James Liddle on 22/09/2018.
+//  Copyright © 2018 appCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
